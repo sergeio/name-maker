6ede8536d59c2369d54b2652e8593921d71266f8
Sergei Orlov <sorlov+github@gmail.com>
2015-01-07 22:23:24 -0500

    hello.cljs: Clean up code, delete commented out things

--- src-cljs/example/hello.cljs
+++ src-cljs/example/hello.cljs
@@ -1,14 +1,8 @@
 (ns example.hello
   (:require [example.names :as names]
-            ;[clojure.browser.dom :as dom]))
             [goog.dom :as dom]))
 
-(def guy-names '("Michael", "Jacob"))
 (def guy-names names/guy-names)
-
-;(js/alert names.guy-names)
-;(.log js/console "hi")
-;(.log js/console names/guy-names)
 
 (defn member? [target collection]
   "Is the target a member of the collection?"
@@ -27,20 +21,23 @@
 (defn string->seq [string] (re-seq #"." string))
 
 (defn get-names-containing [letter guy-names]
+  "Get all the names that contain the desired letter."
   (filter #(member? letter (string->seq %)) guy-names))
 
-;(.log js/console (rand-nth (get-names-containing "J" guy-names)))
-
 (defn vector->name [letter-vector]
+  "Turn vector of names into a string, dropping the last '$' character."
   (reduce str (reverse (rest (reverse letter-vector)))))
 
 (defn append-another-letter [letter-vector guy-names]
+  "Add a letter to the `letter-vector`."
   (let [last-letter (last letter-vector)]
     (conj letter-vector
-          (get-letter-after last-letter
-                            (rand-nth (get-names-containing last-letter guy-names))))))
+          (get-letter-after
+            last-letter
+            (rand-nth (get-names-containing last-letter guy-names))))))
 
 (defn build-name []
+  "Create a name that has structure similar to `guy-names`."
   (let [first-letter (first (rand-nth guy-names))]
     (loop [letter-vector (vector first-letter)]
       (if (= "$" (last letter-vector))
@@ -48,15 +45,4 @@
         (recur (append-another-letter letter-vector guy-names))))))
 
 ;http://docs.closure-library.googlecode.com/git/namespace_goog_dom.html
-
 (dom/setTextContent (dom/getElementByClass "header") (build-name))
-;(.log js/console (dom/getElementByClass "header"))
-;(dom/getElementByClassName "w")
-
-;(dom/append "woo")
-;(let [div (.createElement js/document "DIV")]
-;  (.appendChild div (.createElement js/document "input")))
-
-;(.log ks/console (reduce str (build-name [])))
-(.log js/console (build-name))
-;(.log js/console (reduce str (take 2 (rand-nth guy-names))))
